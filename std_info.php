6d325a8971bf554d5a1cbf90b40142bff0ad332b
janakiraman <janakiraman4103@gmail.com>
2024-09-18 21:47:39 +0530

    nav recreated

--- std_info.php
+++ std_info.php
@@ -25,14 +25,16 @@ if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
 
-$sql = "SELECT regno, department, attendance,present,abscent FROM stdinfo WHERE regno='$reg_no' and department='$dept'  ";
+$sql = "SELECT * FROM stdinfo WHERE regno='$reg_no' and department='$dept'  ";
 $result = mysqli_query($conn, $sql);
 $count = mysqli_num_rows($result);
 $row = mysqli_fetch_assoc($result);
 
     echo" <div>
     <label>REGISTER NUMBER:</label> <span>".$row["regno"]."</span><br><br>
+    <label>NAME:</label> <span>".$row["name"]."</span><br><br>
     <label>DEPARTMENT:</label><span>".$row["department"]."</span><br><br>
+    <label>YEAR:</label> <span>".$row["year"]."</span><br><br>
     <label>NO.OF DAYS PRESENT:</label> <span>".$row["present"]."</span><br><br>
     <label>NO.OF DAYS ABSCENT:</label> <span>".$row["abscent"]."</span><br><br> 
     <label>ATTENDANCE % :</label> <span>".$row["attendance"]."</span><br><br>
