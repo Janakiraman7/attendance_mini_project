517ef4f187f4af138c9b58d53a8ab6aa35ac0929
janakiraman <janakiraman4103@gmail.com>
2024-09-23 06:57:31 +0530

    small changes

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 function add()
 {
     document.getElementById("recordform").action="add.php";
-    document.getElementById("add").style="background-color:green";
+   
 }
 function deleterecord()
 {
     document.getElementById("recordform").action="delete.php";
-    document.getElementById("delete").style="background-color:green";
 }
