3b4739fc17ad4fceac22185f820c343905b9ba5b
janakiraman <janakiraman4103@gmail.com>
2024-09-25 21:56:32 +0530

    time table completed

--- gettimetable.php
+++ gettimetable.php
@@ -4,14 +4,17 @@
     
    
     $con=mysqli_connect("localhost","root","","miniproject");
-    $sql="SELECT * from timetable WHERE year='$year' and department='$department' "  ;
+    $sql="SELECT * from timetable WHERE year=$year and department='$department' "  ;
     $result=mysqli_query($con,$sql);
-    
     $data=array();
-    if(mysqli_num_rows($result)>0){
-        $timetable=mysqli_fetch_assoc($result);
+    $count=mysqli_num_rows($result);
+    if($count>0){
         $data['status']='ok';
-        $data['result']=$timetable;
+        for($i=1;$i<=$count;$i++){
+            $timetable=mysqli_fetch_assoc($result);
+            $data['result'.$i]=$timetable;
+        }
+       
     }else{
         $data['status']='err';
         $data['result']='';
--- script.js
+++ script.js
@@ -22,21 +22,27 @@ $(document).ready(function(){
                 type:"POST",
                 dataType:"json",
                 data:{dept:department,yr:year},
-                beforeSend:function(){
-                    alert(department);
-                },
+                
+            
                 success:function(data){
 
                     if(data['status']=='ok')
                     {
-                        alert("data retrived");
-                        $('#day').text(data.result.day);
-                        $('#one').text(data.result.period1);
-                        $('#two').text(data.result.period2); 
-                        $('#three').text(data.result.period3);
-                        $('#four').text(data.result.period4);
-                        $('#five').text(data.result.period5);
-                        $('#six').text(data.result.period6);
+                        document.getElementById("hidetable").style.display="block";
+                        $('#m').text(data.result1.day);
+                        $('#m1').text(data.result1.period1);
+                        $('#m2').text(data.result1.period2); 
+                        $('#m3').text(data.result1.period3);
+                        $('#m4').text(data.result1.period4);
+                        $('#m5').text(data.result1.period5);
+                        $('#m6').text(data.result1.period6);
+                        $('#t').text(data.result2.day);
+                        $('#t1').text(data.result2.period1);
+                        $('#t2').text(data.result2.period2); 
+                        $('#t3').text(data.result2.period3);
+                        $('#t4').text(data.result2.period4);
+                        $('#t5').text(data.result2.period5);
+                        $('#t6').text(data.result2.period6);
         
                     }
                 }
