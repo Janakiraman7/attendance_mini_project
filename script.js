891295076f4407e885a40dd60ff4af25aa5b04bc
janakiraman <janakiraman4103@gmail.com>
2024-09-25 18:08:50 +0530

    timetable fetch ajax created

--- script.js
+++ script.js
@@ -11,3 +11,27 @@ function updaterecord()
 {
     document.getElementById("recordform").action="update.php";
 }
+function gettimetable(){
+    var year=$('#year').val();
+    var dept=$('#ydepartment').val();
+    $.ajax({
+        url:"getattendance.php",
+        type:"post",
+        datType:"json",
+        data:{dapartment:dept,year:year},
+        success:function(data){
+            if(data.status=='ok')
+            {
+                $('#day').text(data.result.day);
+                $('#one').text(data.result.day);
+                $('#two').text(data.result.day); 
+                $('#three').text(data.result.day);
+                $('#four').text(data.result.day);
+                $('#five').text(data.result.day);
+                $('#six').text(data.result.day);
+
+            }
+        }
+        
+    });
+}
