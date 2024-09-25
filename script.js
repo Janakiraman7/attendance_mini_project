e9e6411df6a07cc055174231c3e60a6b91d031ea
janakiraman <janakiraman4103@gmail.com>
2024-09-25 21:58:01 +0530

    remain rows added

--- script.js
+++ script.js
@@ -43,6 +43,27 @@ $(document).ready(function(){
                         $('#t4').text(data.result2.period4);
                         $('#t5').text(data.result2.period5);
                         $('#t6').text(data.result2.period6);
+                        $('#W').text(data.result2.day);
+                        $('#W1').text(data.result3.period1);
+                        $('#W2').text(data.result3.period2); 
+                        $('#W3').text(data.result3.period3);
+                        $('#W4').text(data.result3.period4);
+                        $('#W5').text(data.result3.period5);
+                        $('#W6').text(data.result3.period6);
+                        $('#th').text(data.result4.day);
+                        $('#th1').text(data.result4.period1);
+                        $('#th2').text(data.result4.period2); 
+                        $('#th3').text(data.result4.period3);
+                        $('#th4').text(data.result4.period4);
+                        $('#th5').text(data.result4.period5);
+                        $('#th6').text(data.result4.period6);
+                        $('#f').text(data.result5.day);
+                        $('#f1').text(data.result5.period1);
+                        $('#f2').text(data.result5.period2); 
+                        $('#f3').text(data.result5.period3);
+                        $('#f4').text(data.result5.period4);
+                        $('#f5').text(data.result5.period5);
+                        $('#f6').text(data.result5.period6);
         
                     }
                 }
